774897eb894de555d02dd5f491deb321eb046580
Nicoleta Ciausu <nixikunn@gmail.com>
2018-06-28 00:07:23 +0300

    Started working on quiz feature! ;D

--- Assets/Scripts/learnScene/toggleSelect.cs
+++ Assets/Scripts/learnScene/toggleSelect.cs
@@ -24,28 +24,44 @@ public class toggleSelect : MonoBehaviour {
 		state = !state;
 		groupToShow.SetActive (state);
 		enabledCenters [shownGroups] = state;
-		cameraCenter.transform.position = recenter ();
+		//cameraCenter.transform.position = recenter ();
 	}
 
-	public Vector3 recenter()
-	{
-		Vector3 newPos = new Vector3 ();
+//	public Vector3 recenter()
+	//{
+		///TODO FIND A WAY TO MAKE THIS WORK
+		/*Vector3 minPoints = new Vector3 (999f,999f,999f);
+		Vector3 maxPoints = new Vector3 (-999f,-999f,-999f);
 		int count = 0;
 		for (int i = 1; i <= shownGroups; i++) {
 			if (enabledCenters [i] == true) {
 			
-				newPos.x += centers [i].localPosition.x;
-				newPos.y += centers [i].localPosition.y;
-				newPos.z += centers [i].localPosition.z;
-				count++;
+				//min
+				//if (centers [i].position.x < minPoints.x)
+				//	minPoints.x = centers [i].position.x;
+				if (centers [i].position.y < minPoints.y)
+					minPoints.y = centers [i].position.y;
+			//	if (centers [i].position.z < minPoints.z)
+			//		minPoints.z = centers [i].position.z;
+
+				//max
+				//if (centers [i].position.x > minPoints.x)
+				//	maxPoints.x = centers [i].position.x;
+				if (centers [i].position.y > minPoints.y)
+					maxPoints.y = centers [i].position.y;
+			//	if (centers [i].position.z > minPoints.z)
+			//		maxPoints.z = centers [i].position.z;
 			}
 		}
-		newPos.x /= count;
-		newPos.y += 4.5f;
-		newPos.y /= count;
+		Vector3 newPos = new Vector3 ();
+		newPos = (minPoints + maxPoints) / 2;
+		//newPos.y += 2.5f;
+		newPos.x = cameraCenter.transform.position.x;
+		newPos.z = cameraCenter.transform.position.z;
 
-		newPos.z /= count;
+
 
 		return newPos;
-	}
+		*/
+	//}
 }
